3515e147897fc13ce843e612f4f6c4b3453b31f5
Kalu Ufere <dekalusha@gmail.com>
2024-08-03 12:54:45 +0100

    modified

--- react-redux/src/components/StripePayment.jsx
+++ react-redux/src/components/StripePayment.jsx
@@ -11,7 +11,7 @@ import {
 } from '@stripe/react-stripe-js';
 import axios from 'axios';
 
-const stripePromise = loadStripe('pk_test_51GyLKTD8bKf8QQtz5VPgmCbpvqrXJgAUMNIXkz41l8iqnYymMCPo9ePEDhMiFRcMXpoQzXIjw7F8WKjq7XhYVwtY00skOdOq55');
+const stripePromise = loadStripe(process.env.STRIPE_PUBLISHABLE_KEY);
 
 const ELEMENT_OPTIONS = {
   style: {
